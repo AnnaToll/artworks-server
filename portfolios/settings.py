ddcd51320b1a6925d7f7cf30d58eeb229c094df8
Anna Toll <anna.toll@protonmail.com>
2023-05-22 16:34:37 +0200

    Added Set-cookie to expose headers

--- portfolios/settings.py
+++ portfolios/settings.py
@@ -87,6 +87,7 @@ app_orgins = [
 
 CORS_ALLOWED_ORIGINS = app_orgins
 CORS_ALLOW_CREDENTIALS = True
+CORS_EXPOSE_HEADERS = "Set-Cookie"
 
 # if IS_HEROKU:
 #     CSRF_COOKIE_DOMAIN = 'gunilla-arno-toll-server.herokuapp.com'
